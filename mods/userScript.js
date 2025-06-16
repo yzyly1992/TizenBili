6c51562b4600f3c861e780cdce5c2fa3c2aeaec3
David Yang <yang.zhiyu@northeastern.edu>
2025-06-15 20:07:40 -0700

    added modernUI back

--- mods/userScript.js
+++ mods/userScript.js
@@ -3,7 +3,7 @@ import './domrect-polyfill';
 import './ui.js';
 import './speedUI.js';
 import './theme.js';
-// import './modernUI.js';
+import './modernUI.js';
 import './infinite-hd.js';
 import './remove-login-popup.js';
 // import './load-comments.js';
